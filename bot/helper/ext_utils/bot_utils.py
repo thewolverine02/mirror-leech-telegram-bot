fe28216e383d224fdcf313bfa54cb936f704586e
thewolverine02 <93479218+thewolverine02@users.noreply.github.com>
2022-02-04 22:46:31 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -125,7 +125,7 @@ def get_readable_message():
                 globals()['PAGE_NO'] -= 1
             START = COUNT
         for index, download in enumerate(list(download_dict.values())[START:], start=1):
-            msg += f"<b>▬▬▬ @LinkZz_MBBS ▬▬▬</b>\n"
+            msg += f"\n\n<b>▬▬▬ @LinkZz_MBBS ▬▬▬</b>\n"
             msg += f"<b>𝗡𝗮𝗺𝗲:</b> <code>{download.name()}</code>"
             msg += f"\n<b>𝗦𝘁𝗮𝘁𝘂𝘀:</b> <i>{download.status()}</i>"
             if download.status() not in [
